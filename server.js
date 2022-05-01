4ec89c9bb16193bda4533202487bdb5814f35d7b
ysaad01 <ysaad2325@gmail.com>
2022-05-01 15:55:42 -0400

    added functions for user input in inquirer prompt

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
 
+const list = require('./utils/list');
+const add = require('./utils/add');
+const update = require('./utils/update');
+
 const promptUser = () => {
   inquirer.prompt([
     {
@@ -19,6 +23,34 @@ const promptUser = () => {
       ]
     }
   ])
+    .then((answers) => {
+      const { choices } = answers
+      // shows all depts
+      if (choices === 'View all Departments') {
+        list.listAllDepartments();
+      }
+      if (choices === 'View all Roles') {
+        list.listAllRoles();
+      }
+      if (choices === 'View all Employees') {
+        list.listAllEmployees();
+      }
+      if (choices === 'Add a Department') {
+        add.addDept();
+      }
+      if (choices === 'Add a Role') {
+        add.addRole();
+      }
+      if (choices === 'Add an Employee') {
+        add.addEmployee();
+      }
+      if (choices === 'Update an Employee') {
+        update.updateEmployee();
+      }
+      if (choices === 'Exit') {
+        db.end();
+      }
+  })
 }
 
 db.connect((err) => {
