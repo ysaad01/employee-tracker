8ec112dedb59c7dd4041e34f96681ecec3b18a96
ysaad01 <ysaad2325@gmail.com>
2022-05-01 01:56:51 -0400

    added addDept() function

--- utils/add.js
+++ utils/add.js
@@ -1,5 +1,34 @@
-/* add a dept */
+const inquirer = require('inquirer');
+const app = require("../server");
+const db = require("../db/connection");
+const { response } = require('express');
 
-/* add a role */
+const add = {
+  // add dept
+  addDept() {
+    inquirer
+      .prompt([
+        {
+          name: "newDept",
+          type: "input",
+          message: "What is the name of the New Dept that you'd like to add?",
+          validate: function validateInput(input) {
+            return input !== "";
+          },
+        },
+      ])
+      // take user input and add new dept to database
+      .then((answer) => {
+        let sql = `INSERT INTO departments (dept_name) VALUES (?)`;
 
-/* add an employee*/
+        db.promise()
+          .query(sql, answer.newDept)
+          .then(() => {
+            console.log("The Dept has been added!");
+            app.promptUser();
+          });
+      });
+  },
+};
+
+module.exports = add; 
