01964e95f78a8e3df2442b97410efcf73124cffa
ysaad01 <ysaad2325@gmail.com>
2022-05-01 17:39:34 -0400

    added listAllDept function to list.js to display All Depts when server.js is ran

--- utils/list.js
+++ utils/list.js
@@ -2,6 +2,22 @@ require("console.table");
 const app = require("../server");
 const db = require("../db/connection");
 
+const list = {
+  // all departments list
+  listAllDepartments() {
+    const sql = `SELECT id AS "ID", dept_name FROM departments`;
 
+    db.promise()
+      .query(sql)
+      .then(([response]) => {
+        console.table(response);
+        app.promptUser();
+      })
+      .catch((error) => console.log(error));
+  },
+  /* all roles list */
+  
+  /* all employees list */
+};
 
 module.exports = list;
