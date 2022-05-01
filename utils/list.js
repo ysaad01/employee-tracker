9e5a49f50857c6387e28660d8a5ddc1863cfbeb9
ysaad01 <ysaad2325@gmail.com>
2022-05-01 18:25:49 -0400

    added listAllEmployees function to display employees in db

--- utils/list.js
+++ utils/list.js
@@ -30,6 +30,22 @@ const list = {
   },
 
   /* all employees list */
+  listAllEmployees() {
+    const sql = `SELECT e.id, e.first_name, e.last_name, title, dept_name AS department, salary,
+                  CONCAT (m.first_name, ' ', m.last_name) as manager
+                  FROM employees e
+                  LEFT JOIN employees m ON e.manager_id = m.id
+                  LEFT JOIN roles r ON e.role_id = r.id
+                  LEFT JOIN departments d ON d.id = dept_id;`;
+
+    db.promise()
+      .query(sql)
+      .then(([response]) => {
+        console.table(response);
+        app.promptUser();
+      })
+      .catch((error) => console.log(error));
+  },
 };
 
 module.exports = list;
