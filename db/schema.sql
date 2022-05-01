029b08e901a1cf004d55156ed17fa70439227b2a
ysaad01 <ysaad2325@gmail.com>
2022-04-30 23:09:52 -0400

    created tables depts, roles and employees

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE departments (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  dept_id INTEGER,
+  CONSTRAINT fkDept_id FOREIGN KEY (dept_id) REFERENCES departments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER,
+  CONSTRAINT fkRole_id FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+  CONSTRAINT fkMgr_id FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+);
